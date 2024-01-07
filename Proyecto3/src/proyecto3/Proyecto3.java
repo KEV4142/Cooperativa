6035d5e054f4c1065feed87facf73787798c358c
KEV4142 <60557934+KEV4142@users.noreply.github.com>
2024-01-07 17:40:48 -0600

    Add files via upload

---
+++ Proyecto3/src/proyecto3/Proyecto3.java
@@ -0,0 +1,11 @@
+package proyecto3;
+import Controller.frmCon;
+/**
+ *
+ * @author root4142
+ */
+public class Proyecto3 {
+    public static void main(String[] args) {
+        new frmCon().setVisible(true);
+    }
+}
